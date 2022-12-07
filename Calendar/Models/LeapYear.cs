c117cb0f94e60b00791bfd437f6940d87057e9f9
Brian <106576147+noh24@users.noreply.github.com>
2022-12-06 23:18:18 -0500

    Initial commit

---
+++ Calendar/Models/LeapYear.cs
@@ -0,0 +1,21 @@
+namespace Calendar.Models
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      if (year % 400 == 0)
+      {
+        return true;
+      }
+      else if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }
+    }
+  }
+}
